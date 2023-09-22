dca7c808d274ea55b4a64479e37b28e7d4f6cd18
Fabien TREGAN <fabien.tregan.ext@sncf.fr>
2023-09-22 13:11:03 +0200

    fixes infinite delay loop while debugging

--- atari2600dump.ino
+++ atari2600dump.ino
@@ -915,6 +915,7 @@ void waitForCartridge() {
 }
 
 void setup() {
+  DWTInitTimer();
   EEPROM8_init(128);
   hotplug = !EEPROM8_getValue(NO_HOTPLUG);
   stellaExt = !EEPROM8_getValue(NO_STELLAEXT);
@@ -993,6 +994,3 @@ void loop() {
   }
   MassStorage.loop();
 }
-
-
-
